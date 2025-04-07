3d12a7a379153df877bd39904b57723164f66305
Crinsay <martyna.loboda@outlook.com>
2025-04-07 19:56:49 +0200

    feature: added product and negotiation entities

---
+++ Negotiations.Domain/Entities/Negotiation.cs
@@ -0,0 +1,10 @@
+﻿namespace Negotiations.Domain.Entities;
+
+public class Negotiation
+{
+    public int Id { get; set; }
+    public int ProductId { get; set; } 
+    public decimal SuggestedPrice { get; set; }
+    public bool? IsAccepted { get; set; }
+    public DateTime? DeclineDate { get; set; }
+}
