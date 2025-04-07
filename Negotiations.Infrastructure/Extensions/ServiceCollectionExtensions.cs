4fec55196eff391fe9b0839e4739b01193b2d3d2
Crinsay <martyna.loboda@outlook.com>
2025-04-07 20:32:34 +0200

    feature: added database

---
+++ Negotiations.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Negotiations.Infrastructure.Persistence;
+
+namespace Negotiations.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("NegotiationsDb");
+        services.AddDbContext<NegotiationsDbContext>(options =>
+                options
+                    .UseSqlServer(connectionString)
+                    .EnableSensitiveDataLogging());
+    }
+
+}
